531822cf54357965e20dc7ff1223ed606483b74d
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2024-06-01 15:21:36 -0700

    Implemented playRound function

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function getComputerChoice() {
     } else if (choiceNumber >= 0.34 && choiceNumber < 0.67) {
         return "Paper";
     } else {
-        return "Scissor";
+        return "Scissors";
     }
 }
 
 function getHumanChoice() {
     let input = prompt("Type Rock, Paper, or Scissors to make your selection");
-    let standardInput = input.toLocaleLowerCase;
+    let standardInput = input.toLocaleLowerCase().trim();
     if (standardInput == "rock") {
         return "Rock";
     } else if (standardInput == "paper") {
@@ -20,5 +20,34 @@ function getHumanChoice() {
         return "Scissors";
     } else {
         return "Invalid Input";
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("It's a tie! No winner this round.");
+    } else if (humanChoice == "Rock" && computerChoice == "Paper") {
+        console.log("You lose! Paper beats rock.");
+        computerScore += 1;
+    } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
+        console.log("You lose! Scissors beats paper.");
+        computerScore += 1;
+    } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+        console.log("You lose! Rock beats scissors.");
+        computerScore += 1;
+    } else if (computerChoice == "Rock" && humanChoice == "Paper") {
+        console.log("W! Paper beats rock.");
+        humanScore += 1;
+    } else if (computerChoice == "Paper" && humanChoice == "Scissors") {
+        console.log("W! Scissors beats paper.");
+        humanScore += 1;
+    } else if (computerChoice == "Scissors" && humanChoice == "Rock") {
+        console.log("W! Rock beats scissors.");
+        humanScore += 1;
+    } else if (humanChoice == "Invalid Input") {
+        console.log("Invalid Input, restart round.");
     }
 }
